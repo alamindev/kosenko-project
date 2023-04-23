de19ffdf9839a21286f267ca408ad0b7403b1f55
md alamin <mdalamin@mds-MacBook-Pro.local>
2023-04-23 23:35:22 +0600

    added section 3 pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      nunito_sans: ["Nunito Sans", "sans-serif"], 
+      nunito_sans: ["Nunito Sans", "sans-serif"],
     },
     extend: {
       colors: {
-        "dark": "#1f1f1f",  
-        "brand-orange": "#f16035",  
-        "brand-gray": "#eaeaea",  
-        "brand-gray-100": "#e2e2e2",  
-        "brand-gray-200": "#bababa",  
-      }, 
+        dark: "#1f1f1f",
+        "brand-orange": "#f16035",
+        "brand-gray": "#eaeaea",
+        "brand-gray-100": "#e2e2e2",
+        "brand-gray-200": "#bababa",
+        "brand-gray-300": "#b9b9b9",
+        "brand-green": "#59a935",
+      },
       screens: {
-        '2xl': '1410px'
-      }
+        "2xl": "1410px",
+      },
     },
   },
   plugins: [
